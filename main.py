709bc91654d53f4a73e26ac2a0d8d1c012d94a5d
VitaliLuca <vitalilucaster@gmail.com>
2020-04-18 09:53:10 +0200

    Premier commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import pygame
+import os.path
+pygame.init()
+
+#Import des modules contenant les classes que l'on va instancier
+import objects
+
+class Game:
+    def __init__(self,plaSpr,astSpr,window_size,level=1):
+        self.window_size=window_size
+        self.playerSprite=plaSpr
+        self.asteroidSprite=astSpr
+        self.score=0
+        self.StartLevel(level)
+
+    def StartLevel(self,level): #On instancie les objets au début de niveau        
+        self.player = objects.Player(self.playerSprite,self.window_size[0]/2,self.window_size[1]/2)
+        self.asteroids = []
+        for i in range(2*level):
+            self.asteroids.append(objects.Asteroid(self.asteroidSprite,self.window_size,1))
+
+    def GameDraw(self,win):    #Cette fonction va dessiner chaque élément du niveau
+        self.player.Draw(win)
+        for asteroid in self.asteroids:
+            asteroid.Draw(win)
+
+class App:
+    def __init__(self):
+        folder = os.path.dirname(__file__)
+        self.window_size=[1280,720]
+        self.window = pygame.display.set_mode((self.window_size[0],self.window_size[1]))
+        plaSpr = pygame.image.load(os.path.join(folder, 'Assets/player.png'))
+        astSpr = pygame.image.load(os.path.join(folder, 'Assets/rock.png'))
+        self.game = Game(plaSpr,astSpr,self.window_size)    #Sera instancié quand on clique sur NEW GAME
+
+        self.running = True        
+        while self.running:  #Boucle qui exécute et affiche le jeu + vérifie les inputs
+            pygame.time.delay(100)
+            self.Events()   #Gestion des évènements/inputs/clics
+            self.window.fill((0,0,0)) #Vide l'affichage de la frame
+            self.FrameDraw() #Appelle la fonction qui dessine les objets du jeu
+            pygame.display.update() #Met à jour l'affichage
+        pygame.quit()
+
+    def Events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    print('LClick')
+                    #player.Shoot()
+            elif event.type == pygame.KEYDOWN:
+                #player.Key(event.key)
+                pass
+
+    def FrameDraw(self):    #Cette fonction va dessiner chaque élément du programme
+        self.game.GameDraw(self.window)
+
+if __name__ == "__main__":  #Instancie le programme
+    app = App()
---
+++ objects.py
@@ -0,0 +1,60 @@
+import pygame
+import random
+
+class Player:
+
+    def __init__(self,sprite,x,y):
+        self.sprite=sprite
+        self.x=x
+        self.y=y
+        self.size=50
+        self.angle=90
+
+    def Key(self,key):
+        if (key==pygame.K_RIGHT):
+            print('right')
+
+    def Shoot(self):
+        pass
+
+    def Teleport(self):
+        if (True) :
+            #TP()
+            pass
+        else :
+            Teleport()
+
+    def Draw(self,window):
+        window.blit(self.sprite,(self.x-self.size/2,self.y-self.size/2))
+
+class Asteroid:
+
+    def __init__(self,sprite,window_size,initialType,x=None,y=None): #Constructeur de l'objet
+        self.sprite=sprite
+        self.size=initialType
+        self.size=0
+        if (x is None): #Si on ne passe pas de paramètre, créé aléatoirement sur l'écran
+            self.x=random.randint(0,window_size[0])
+            self.y=random.randint(0,window_size[1])
+        else:   #Sinon on crée l'objet à la bonne position
+            self.x=x
+            self.y=y        
+        self.Appearance(self.sprite)
+
+    def Appearance(self,sprite):    #Dimensionne l'asteroide selon son type et altère la taille pour les rendre uniques
+        if (self.type==1):
+            size=60
+        elif (self.type==2):
+            size=30
+        elif (self.type==3):
+            size=15
+        scale=random.uniform(0.7,1)
+        self.size=int(scale*size)
+        self.sprite=pygame.transform.scale(sprite,(self.size,self.size))
+
+    def Destroy(self):
+        #create 2 new asteroids with type-=1
+        pass
+
+    def Draw(self,window):
+        window.blit(self.sprite,(self.x-self.size/2,self.y-self.size/2))
