ff9303fca925be62d9aa532fe8be2d2ee0c4a54d
MartinV99 <63638650+MartinV99@users.noreply.github.com>
2020-04-19 09:31:24 +0200

    modification Unit_Test.py

--- Unit_Test.py
+++ Unit_Test.py
@@ -1,5 +1,18 @@
-import unittest
+import objects
 
-class Test (unittest.TestCase):
-  
-  def          (self):
+def test_cons_PlayerSpaceShip():
+    """ Test des constantes de la class PlayerSpaceShip : """
+    my_PlayerSpaceShip = objects.PlayerSpaceShip(sprite=2, x=10, y=4)
+    assert type(my_PlayerSpaceShip.sprite) is int or float
+    assert type(my_PlayerSpaceShip.x) is int or float
+    assert type(my_PlayerSpaceShip.y) is int or float
+    assert type(my_PlayerSpaceShip.angle) is int or float
+    assert type(my_PlayerSpaceShip.speed) is int or float
+    assert type(my_PlayerSpaceShip.acceleration) is int or float
+    assert type(my_PlayerSpaceShip.size) is int or float
+    assert type(my_PlayerSpaceShip.life) is int or float
+    assert type(my_PlayerSpaceShip.shoot_rate) is int or float
+    assert type(my_PlayerSpaceShip.type) is int or float
+    assert my_PlayerSpaceShip.sprite == 2
+    assert my_PlayerSpaceShip.x == 10
+    assert my_PlayerSpaceShip.y == 4
