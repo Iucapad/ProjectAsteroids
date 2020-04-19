0c914bca81815bc4f78263465f95dc0db15fae1e
MartinV99 <63638650+MartinV99@users.noreply.github.com>
2020-04-19 09:40:08 +0200

    Correction

--- Unit_Test.py
+++ Unit_Test.py
@@ -2,7 +2,7 @@ import objects
 
 def test_cons_PlayerSpaceShip():
     """ Test des constantes de la class PlayerSpaceShip : """
-    my_PlayerSpaceShip = objects.PlayerSpaceShip(sprite=2, x=10, y=4)
+    my_PlayerSpaceShip = objects.PlayerSpaceShip(sprite="Player", x=10, y=4)
     assert type(my_PlayerSpaceShip.sprite) is int or float
     assert type(my_PlayerSpaceShip.x) is int or float
     assert type(my_PlayerSpaceShip.y) is int or float
@@ -13,6 +13,6 @@ def test_cons_PlayerSpaceShip():
     assert type(my_PlayerSpaceShip.life) is int or float
     assert type(my_PlayerSpaceShip.shoot_rate) is int or float
     assert type(my_PlayerSpaceShip.type) is int or float
-    assert my_PlayerSpaceShip.sprite == 2
+    assert my_PlayerSpaceShip.sprite == "Player"
     assert my_PlayerSpaceShip.x == 10
     assert my_PlayerSpaceShip.y == 4
